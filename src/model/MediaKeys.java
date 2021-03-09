fb6a695845eb688bf0d3567a953b0b1fa0ea94f3
Branden <branden_2229@hotmail.com>
2021-03-09 14:54:22 -0600

    Added Functionality to MediaKeys.

--- src/model/MediaKeys.java
+++ src/model/MediaKeys.java
@@ -1,4 +1,46 @@
 package model;
 
-public class MediaKeys {
+public class MediaKeys extends Hotkey implements InputEmulator{
+
+    // constant for testing
+
+    // Play/Pause Media Key: 0xB2
+    private static int play = Integer.decode("0xB2");
+    // Mute Media Key: 0xAD
+    private static int mute = Integer.decode("0xAD");
+    // Next Media Key: 0xB0
+    private static int next = Integer.decode("0xB0");
+    // Prev Media Key: 0xB1
+    private static int prev = Integer.decode("0xB1");
+
+
+    /**
+     * Constructs a hotkey
+     * @param keyCode Virtual keycode
+     * @param id id of hotkey
+     * @param modifier modifer for keycode
+     */
+    public MediaKeys (int keyCode, int id, int modifier){
+        super(keyCode, id , modifier);
+    }
+
+
+
+    @Override
+    public void sendKey(int keyCode, boolean release){
+
+        if(keyCode == play){
+            System.out.println("sending Play keycode" + keyCode);
+        }
+        else if (keyCode == mute){
+            System.out.println("sending mute keycode" + keyCode);
+        }
+        else if (keyCode == next){
+            System.out.println("sending next keycode" + keyCode);
+        }
+        else if (keyCode == prev){
+            System.out.println("sending prev keycode" + keyCode);
+        }
+    }
+
 }
