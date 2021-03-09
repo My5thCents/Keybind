555c110f987f0de2a26dae6279af9f01c9c0ea58
Jason <jcd763@mail.usask.ca>
2021-03-08 18:04:47 -0600

    Values added to keyValueMap, currently printing to console for unit testing. Not linked to backend yet.

--- src/ui/OptionsList.java
+++ src/ui/OptionsList.java
@@ -13,27 +13,27 @@ public class OptionsList {
                 "Q ", "W ", "E ", "R ", "T ", "Y ", "U ", "I ", "O ", "P ", "A ", "S ", "D ", "F ",
                 "G ", "H ", "J ", "K ", "L ", "Z ", "X ", "C ", "V ", "B ", "N ", "M ", ". Period",
                 ", Comma", "/ Back slash", "Right shift", "[ Left square bracket", "] Right square bracket",
-                "\\ Forward slash", "Esc", "F1", "F2", "F3", "F4", "F5", "F6", "F7", "F8", "F9", "F10", "F11", "F12", "` Tilde",
+                "\\ Forward slash", "Esc", "F1 ", "F2 ", "F3 ", "F4 ", "F5 ", "F6 ", "F7 ", "F8 ", "F9 ", "F10 ", "F11 ", "F12 ", "` Tilde",
                 "1 ", "2 ", "3 ", "4 ", "5 ", "6 ", "7 ", "8 ", "9 ", "0 ", "- Dash", "= Equals", "Backspace", "Tab",
-                "Caps Lock", "Enter", "Left Shift", "Left Control", "Left Alt", "Right Alt", "Insert", "Home",
+                "Caps Lock", "Enter", "Left Shift", "Left Control", "Alt", "Insert", "Home",
                 "Page Up", "Delete", "End", "Page Down", "Left Arrow", "Up Arrow", "Down Arrow", "Right Arrow",
-                "Number Lock", "Numpad slash", "Numpad asterisk", "Numpad dash", "Numpad 7", "Numpad 8", "Numpad 9",
-                "Numpad + plus", "Numpad 4", "Numpad 5", "Numpad 6", "Numpad 1", "Numpad 2", "Numpad 3", "Numpad enter",
-                "Numpad 0", "Numpad . period", "Mouse button 4", "Mouse button 5"
+                "Number Lock", "/ Numpad slash", "* Numpad asterisk", "- Numpad dash", "Numpad 7", "Numpad 8", "Numpad 9",
+                "+ Numpad plus", "Numpad 4", "Numpad 5", "Numpad 6", "Numpad 1", "Numpad 2", "Numpad 3",
+                "Numpad 0", ". Numpad period", "Mouse button 4", "Mouse button 5"
         };
         //List of options the user can select as actions for the keybind
         optionsActions = new String[] {
                 "Q ", "W ", "E ", "R ", "T ", "Y ", "U ", "I ", "O ", "P ", "A ", "S ", "D ", "F ",
                 "G ", "H ", "J ", "K ", "L ", "Z ", "X ", "C ", "V ", "B ", "N ", "M ", ". Period",
                 ", Comma", "/ Back slash", "Right shift", "[ Left square bracket", "] Right square bracket",
-                "\\ Forward slash", "Esc", "F1", "F2", "F3", "F4", "F5", "F6", "F7", "F8", "F9", "F10", "F11", "F12", "` Tilde",
+                "\\ Forward slash", "Esc", "F1 ", "F2 ", "F3 ", "F4 ", "F5 ", "F6 ", "F7 ", "F8 ", "F9 ", "F10 ", "F11 ", "F12 ", "` Tilde",
                 "1 ", "2 ", "3 ", "4 ", "5 ", "6 ", "7 ", "8 ", "9 ", "0 ", "- Dash", "= Equals", "Backspace", "Tab",
-                "Caps Lock", "Enter", "Left Shift", "Left Control", "Left Alt", "Right Alt", "Insert", "Home",
+                "Caps Lock", "Enter", "Left Shift", "Left Control", "Alt", "Insert", "Home",
                 "Page Up", "Delete", "End", "Page Down", "Left Arrow", "Up Arrow", "Down Arrow", "Right Arrow",
-                "Number Lock", "Numpad slash", "Numpad asterisk", "Numpad dash", "Numpad 7", "Numpad 8", "Numpad 9",
-                "Numpad + plus", "Numpad 4", "Numpad 5", "Numpad 6", "Numpad 1", "Numpad 2", "Numpad 3", "Numpad enter",
-                "Numpad 0", "Numpad . period", "Microphone mute/unmute", "Volume mute/unmute", "Media play/pause",
-                "Media next track", "Media previous track", "Mouse up", "Mouse down", "Mouse left", "Mouse right",
+                "Number Lock", "/ Numpad slash", "* Numpad asterisk", "- Numpad dash", "Numpad 7", "Numpad 8", "Numpad 9",
+                "+ Numpad plus", "Numpad 4", "Numpad 5", "Numpad 6", "Numpad 1", "Numpad 2", "Numpad 3",
+                "Numpad 0", ". Numpad period", "Volume mute/unmute", "Media play/pause",
+                "Media next track", "Media previous track"
         };
     }
 
