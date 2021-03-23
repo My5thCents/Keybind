fd874a4b0d71bc598fadffab46e32d5adf654f37
Branden <branden_2229@hotmail.com>
2021-03-22 21:52:02 -0600

    Added runApplication method

--- src/model/LaunchApplication.java
+++ src/model/LaunchApplication.java
@@ -1,4 +1,23 @@
 package model;
+import java.awt.*;
+import java.io.File;
+import java.io.IOException;
 
 public class LaunchApplication {
+
+    public static void runApplication(String FilePath){
+        File application = new File(FilePath);
+        String Name = application.getName();
+
+        try {
+            boolean alreadyRunning = DetectProgram.isRunning(Name);
+            if(alreadyRunning == false){
+                Desktop.getDesktop().open(application);
+            }
+        }
+        catch (IOException ex){
+            System.out.println(ex.toString());
+        }
+    }
+
 }
