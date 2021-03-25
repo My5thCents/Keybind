c1f6c2d32db24b73ffc09ee962785c8fe39d98a9
djn011 <djn011@mail.usask.ca>
2021-03-24 22:08:15 -0600

    Finished saveEverything Created mouseAction and mouseEmulator -More testing is needed

--- src/model/mouseEmulator.java
+++ src/model/mouseEmulator.java
@@ -1,50 +1,28 @@
 package model;
 
+import Controller.mouseAction;
+
 public class mouseEmulator{
-    //Directions can be: L(left),R(right),U(up),D(down)
-    private final String direction;
+    protected int keyCode;
+    protected int id;
+    protected final mouseAction action;
+
+    public mouseEmulator(String dir, int keycode, int id ) {
+        this.action = new mouseAction(dir);
+        this.keyCode = keycode;
+        this.id = id;
+    }
     /**
-     * Constructs an mouseEmulator for a hotkey.
-     * @param d the direction in which the mouse should move
+     * Gets the virtual keyCode.
      */
-    public mouseEmulator(String d) {
-        direction = d;
+    public int getKeyCode() {
+        return keyCode;
     }
 
-    public String move(){
-        if(direction.equals("L")){
-            OSInterface.getInstance().moveMouse(5,0);
-            return "Moved left\n";
-        }
-        else if(direction.equals("R")){
-            OSInterface.getInstance().moveMouse(-5,0);
-            return "Moved right\n";
-        }
-        else if(direction.equals("U")){
-            OSInterface.getInstance().moveMouse(0,5);
-            return "Moved up\n";
-        }
-        else if(direction.equals("D")){
-            OSInterface.getInstance().moveMouse(0,-5);
-            return "Moved down\n";
-        }
-        return "Not moved\n";
-    }
-    public static void main(String[] args){
-        mouseEmulator test = new mouseEmulator("L");
-        mouseEmulator test2 = new mouseEmulator("R");
-        mouseEmulator test3 = new mouseEmulator("U");
-        mouseEmulator test4 = new mouseEmulator("D");
-        mouseEmulator test5 = new mouseEmulator("E");
-        System.out.println(test.direction);
-        System.out.println(test2.direction);
-        System.out.println(test3.direction);
-        System.out.println(test4.direction);
-        System.out.print(test.move());
-        System.out.print(test2.move());
-        System.out.print(test3.move());
-        System.out.print(test4.move());
-        System.out.println(test5.move());
-
+    /**
+     * Gets the unique ID.
+     */
+    public int getID() {
+        return id;
     }
 }
