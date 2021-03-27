31107f06788502c8d6fdd0a5c018dac471c37a51
Branden <branden_2229@hotmail.com>
2021-03-27 15:39:08 -0600

    Created StringToInt method for converting string to actionlist

--- src/model/ProgramLaunchConverter.java
+++ src/model/ProgramLaunchConverter.java
@@ -1,4 +1,33 @@
 package model;
 
+import java.util.ArrayList;
+
 public class ProgramLaunchConverter {
+
+    /**
+     * Takes the file path as a string and converts it to an ArrayList<Integer>. the first entry is always 10000, this is to identify it has a launch program hotkey.
+     * @param s a string that represents the absolute path to the exe you wish to bind.
+     * @return and ArrayList<Integer> of actions.
+     */
+    public static ArrayList<Integer> StringToInt(String s){
+        ArrayList<Integer> actionList = new ArrayList<>();
+        // Identifying keycode used to check during preform action if it's a launch program type.
+        actionList.add(10000);
+
+        try {
+            byte[] bytes = s.getBytes("US-ASCII");
+            //System.out.println(Arrays.toString(bytes));
+
+            for(int i = 0; i< bytes.length; i = i +1){
+                int p = bytes[i];
+                //System.out.println(p);
+                actionList.add(p);
+            }
+        }
+        catch (Exception e){
+            e.printStackTrace();
+        }
+
+        return actionList;
+    }
 }
